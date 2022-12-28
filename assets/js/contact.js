6f463dca272d96320a5c0386e7c2f15d92849529
Jero Rm <jeroidracko@gmail.com>
2022-12-27 23:27:41 -0500

    update form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -38,3 +38,41 @@ document.getElementById('form')
         alert(JSON.stringify(err));
       });
   });
+
+/* CAPTCHA */
+
+const SECRET_KEY = '6Le4K7IjAAAAAKhXutobLAVNkajyxdZcD7l_4Zez';
+
+const form = document.getElementById('form');
+const submitButton = form.querySelector('input[type="submit"]');
+
+submitButton.disabled = true;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  grecaptcha.execute('6Le4K7IjAAAAAKhXutobLAVNkajyxdZcD7l_4Zez', { action: 'submit' }).then(function (token) {
+    const response = token;
+
+    const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${response}`;
+    fetch(verificationUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          form.submit();
+        } else {
+          console.error('Error al verificar el reCAPTCHA');
+          submitButton.disabled = true;
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        submitButton.disabled = true;
+      });
+  });
+});
+
+
+
+
+
