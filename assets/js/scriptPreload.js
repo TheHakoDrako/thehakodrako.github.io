4e03614b1d745b92c4cb3975dbabdae356444e50
Jero Rm <jeroidracko@gmail.com>
2023-01-02 22:22:58 -0500

    update style preload

--- assets/js/scriptPreload.js
+++ assets/js/scriptPreload.js
@@ -1,7 +1,11 @@
 const preload = document.querySelector('.preloadCentrado');
+const wrap = document.querySelector('.wrapperLoad');
+
+wrap.style.display = 'none';
 
 window.addEventListener('load', () => {
     setTimeout(() => {
         preload.style.display = 'none';
+        wrap.style.display = 'block';
     }, 2000);
 });
