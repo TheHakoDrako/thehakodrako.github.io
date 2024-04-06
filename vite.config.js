4a65006d05c0e2b7fc453c7013528167513ef2cc
Jero Rm <88847168+TheHakoDrako@users.noreply.github.com>
2024-04-06 08:36:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jerodev.work.gd/'
+  base: 'jerodev.work.gd/'
 })
