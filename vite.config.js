d1bfc51ee1da8f16232054444f53ded20392a999
Jero Rm <88847168+TheHakoDrako@users.noreply.github.com>
2024-04-08 21:13:51 -0500

    Update vite.config.js
    - Server updated
    - Certificate https (SSL)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jerodev.work.gd/'
-})
+  base: 'https://jerodev.work.gd/',
+  server: {
+    https: {
+      key: fs.readFileSync('./.cert/private.key'),
+      cert: fs.readFileSync('./.cert/certificate.pem'),
+      ca: fs.readFileSync('./.cert/ca_bundle.pem')
+    }
+  }
+});
